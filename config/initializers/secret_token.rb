4820d8b71120c9866b8628e616361a6cc6ed4c69
yaowen.zheng <yaowen.zheng@hulu.com>
2012-04-09 20:50:45 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '70636fffed620d49d1e4b85f3545031058b5cc090b98a9c26b4c7f2ff4cb158e2a10720f1a7e68a92a295084fb226c1a4a5415872252030170773b8c42677ca4'
